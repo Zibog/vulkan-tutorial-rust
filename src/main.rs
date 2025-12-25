e70ee8b4d44c2f4f8206b539421a0afac9f93b09
Dmitry Sidak <dmsidak@gmail.com>
2025-12-25 03:33:56 +0200

    Create Vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@
     clippy::unnecessary_wraps
 )]
 
-use anyhow::{Error, Ok, Result};
+use anyhow::{Error, Ok, Result, anyhow};
+use vulkanalia::{Entry, Instance, Version, loader::{LIBRARY, LibloadingLoader}, vk::{self, HasBuilder, InstanceV1_0}};
+use vulkanalia::window as vk_window;
 use winit::{dpi::LogicalSize, event::{Event, WindowEvent}, event_loop::EventLoop, window::{Window, WindowAttributes}};
+
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -46,12 +50,18 @@ fn main() -> Result<()> {
 
 /// Our Vulkan app
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
     /// Creates our Vulkan app
     unsafe fn create(window: &Window) -> Result<Self, Error> {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
     /// Renders a frame for our Vulkan app
@@ -60,9 +70,31 @@ impl App {
     }
 
     /// Destroys our Vulkan app
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app
 #[derive(Clone, Debug, Default)]
 struct AppData {}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance, Error> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial\0")
+        .application_version(vk::make_version(0, 1, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(0, 1, 0))
+        .api_version(vk::make_version(0, 1, 0));
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}
