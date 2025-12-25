8fe37969df7150199790f20769ce38b3405cb7b7
Dmitry Sidak <dmsidak@gmail.com>
2025-12-25 02:06:18 +0200

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use std::io::{BufWriter, stdout};
-
-use ferris_says::say;
-
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    let message = "Hello fellow Rustaceans!";
+    println!("{}", message);
 }
